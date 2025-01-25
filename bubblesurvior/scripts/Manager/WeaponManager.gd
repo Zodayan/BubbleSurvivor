143e24bf7c397dd2839f9a64ada50714fa487bfe
Zodayan <82103096+Zodayan@users.noreply.github.com>
2025-01-25 22:41:41 +0100

    On peut presque changer d'arme

--- bubblesurvior/scripts/Manager/WeaponManager.gd
+++ bubblesurvior/scripts/Manager/WeaponManager.gd
@@ -5,10 +5,9 @@ var _tree : CustomSceneTree
 
 var _weapon : Weapon
 const listWeapon = [
-"",
-"",
-"",
-""]
+	"res://scenes/fusil_pompe.tscn",
+	"res://scenes/pistolet.tscn"
+]
 
 var currentWeapon : int = 0
 
@@ -18,21 +17,35 @@ func _initialize():
 	print("Weapon Manager init...")
 	_tree = CustomSceneTree.getInstance()
 	
+	# Chargement de l'arme
+	laodWeapon()
+	
 func changeWeapon(weapon):
 	if _weapon :
 		_tree.root.remove_child(_weapon)
 	_weapon = weapon
 	_tree.root.add_child(_weapon)
 
-func nextWeapon():
-	if(currentWeapon>=listWeapon.size()):
-		currentWeapon = 0
-	else :
-		currentWeapon+=1
-	
+func laodWeapon():
 	var weapon = ResourceLoader.load(listWeapon[currentWeapon]).instantiate()
 	if weapon :
 		changeWeapon(weapon)
+
+func prevWeapon():
+	if(currentWeapon <= 0):
+		currentWeapon = listWeapon.size()-1
+	else :
+		currentWeapon -= 1
+	
+	laodWeapon()
+
+func nextWeapon():
+	if(currentWeapon >= listWeapon.size()):
+		currentWeapon = 0
+	else :
+		currentWeapon += 1
+	
+	laodWeapon()
 
 func delWeapon():
 	if _weapon :
