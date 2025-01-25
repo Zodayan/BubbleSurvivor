384b8eb163268f5ebb4a7b5119091e307f3613ac
Dav314159 <davidjavet2@gmail.com>
2025-01-25 23:37:49 +0100

    normalement ca marche trust

--- bubblesurvior/scripts/weapons/pistolet.gd
+++ bubblesurvior/scripts/weapons/pistolet.gd
@@ -2,15 +2,19 @@ extends Weapon
 
 @onready var projectile_scene = preload("res://scenes/projectile_simple.tscn")
 @onready var pistolet = $AnimatedSprite2D
-
+@onready var timer = $Timer
 func _init() -> void:
 	degats = 10
 	vitesse_tir = 5
 	cout_tir = 1
 
+func _process(delta: float) -> void:
+	super._process(delta)
+	position.x += 10
 # A potentiellement toucher avec héritage
 func action_tirer() -> void:
 	pistolet.play("tape")
+	timer.start(0.5)
 	var nouveau_projectile: ProjectileSimple = projectile_scene.instantiate()
 	var ennemi_plus_proche: Enemy = CustomSceneTree.getInstance()._ennemyManager._nearest_enemy(position)
 	nouveau_projectile.arme_associee = self
@@ -22,3 +26,8 @@ func action_tirer() -> void:
 		nouveau_projectile.direction = Vector2(1, 0)
 	nouveau_projectile.position = position
 	CustomSceneTree.getInstance().root.add_child(nouveau_projectile)
+	
+
+
+func _on_timer_timeout() -> void:
+	pistolet.play("default") # Replace with function body.
