68a0025c52b92a1a68b9ebb9f7251ab59e952e47
Pingrammer <108672186+Pingrammer@users.noreply.github.com>
2025-01-25 11:50:00 +0100

    manager

---
+++ bubblesurvior/scripts/Manager/CustomSceneTree.gd
@@ -0,0 +1,37 @@
+class_name CustomSceneTree
+extends SceneTree
+
+var _levelManager : LevelManager 
+var _playerManager : PlayerManager 
+static var _instance : CustomSceneTree
+
+var time_elapsed : float = 0
+
+static func getInstance():
+	
+	return _instance
+
+func _initialize():
+	
+	print("Custom Scene Tree init...")
+	_instance = self
+	_levelManager = LevelManager.new()
+	root.add_child(_levelManager)
+	_levelManager._initialize()
+	_playerManager = PlayerManager.new()
+	root.add_child(_playerManager)
+	_playerManager._initialize()
+	
+	_levelManager.addScene("res://scenes/bubblePlayer.tscn")
+	
+	print("Initialized:")
+	print("  Starting time: %s" % str(time_elapsed))
+
+func _process(delta):
+	time_elapsed += delta
+	# Return true to end the main loop.
+	
+
+func _finalize():
+	print("Finalized:")
+	print("  End time: %s" % str(time_elapsed))
