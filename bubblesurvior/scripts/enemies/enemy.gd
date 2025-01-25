ed361abd2a51d327f6584ae410a0ffcecd5b8166
Zodayan <82103096+Zodayan@users.noreply.github.com>
2025-01-25 14:55:13 +0100

    Les ennemis peuvent apparaître n'importe où dans une zone autour du joueur

--- bubblesurvior/scripts/enemies/enemy.gd
+++ bubblesurvior/scripts/enemies/enemy.gd
@@ -1,7 +1,8 @@
 extends Area2D
 class_name Enemy
 
-const DELAI_DEGATS = 1 #delais de 1s pour infliger à nouveau des dégats au joueur
+const DELAI_DEGATS: float = 1 #delais de 1s pour infliger à nouveau des dégats au joueur
+const DISTANCE_APPARITION: Array[float] = [200, 300]
 var vitesse: float = 10
 var pv_max: float = 10
 var pv: float = pv_max
@@ -10,6 +11,12 @@ var direction: Vector2 = Vector2(1, 0)
 var is_collision_joueur: bool = false
 var timer_collision: float = 0
 var player_body: BubblePlayer
+	
+func _init() -> void:
+	# On fait apparaître l'ennemi autour du joueur
+	var angle: float = randf_range(0, 2*PI)
+	position = Vector2(	cos(angle) * randf_range(DISTANCE_APPARITION[0], DISTANCE_APPARITION[1]),
+						sin(angle) * randf_range(DISTANCE_APPARITION[0], DISTANCE_APPARITION[1]))
 	
 func _process(delta: float) -> void:
 	
@@ -31,9 +38,8 @@ func gerer_collision_joueur(delta: float) -> void:
 		if timer_collision >= DELAI_DEGATS:
 			player_body.dealDamage(degats)
 			print("player hit")
-			timer_collision -= DELAI_DEGATS
-		else:
-			timer_collision += delta
+			timer_collision = 0
+	timer_collision += delta
 
 # Se déclenche quand le joueur rentre en collision avec la collision de ce sprite
 func _on_body_entered(body: Node2D) -> void:
@@ -41,11 +47,9 @@ func _on_body_entered(body: Node2D) -> void:
 	#qui va faire des dégats au joueur toutes les secondes
 	if body.is_class("CharacterBody2D"):
 		is_collision_joueur = true
-		timer_collision = 1
 		player_body = body
 		
 func _on_body_exited(body: Node2D) -> void:
 	
 	if body.is_class("CharacterBody2D"):
 		is_collision_joueur = false
-		timer_collision = 0
