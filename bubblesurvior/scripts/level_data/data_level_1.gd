73d4c88e08db5e26cf74e2af264c38aa2058447b
Zodayan <82103096+Zodayan@users.noreply.github.com>
2025-01-25 17:23:37 +0100

    correction liste ennemis dans level data

--- bubblesurvior/scripts/level_data/data_level_1.gd
+++ bubblesurvior/scripts/level_data/data_level_1.gd
@@ -1,4 +1,4 @@
 extends Object
 
 var score_pour_boss: float = 200
-var liste_ennemis: Dictionary = {"res://scenes/michel.tscn": 5, "res://scenes/gros_michel.tscn": 1}
+var liste_ennemis: Array[Array] = [["res://scenes/michel.tscn", 5], ["res://scenes/gros_michel.tscn", 1]]
