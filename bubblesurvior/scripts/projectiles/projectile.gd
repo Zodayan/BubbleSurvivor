34353f8e7150247d1d404047b91398a85475e943
Zodayan <82103096+Zodayan@users.noreply.github.com>
2025-01-25 15:17:29 +0100

    Creation de la classe de projectile

---
+++ bubblesurvior/scripts/projectiles/projectile.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+var vitesse: float = 1
+var direction: Vector2 = Vector2(1, 0)
+var arme_associee = Weapon
--- bubblesurvior/scripts/weapons/weapon.gd
+++ bubblesurvior/scripts/weapons/weapon.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 class_name Weapon
 
 var degats: float = 1
