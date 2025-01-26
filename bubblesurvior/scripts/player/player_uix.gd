ff7f7a10404aafef24cae9eda895b2c623fd3723
Pingrammer <108672186+Pingrammer@users.noreply.github.com>
2025-01-26 11:26:17 +0100

    maj bar

--- bubblesurvior/scripts/player/player_uix.gd
+++ bubblesurvior/scripts/player/player_uix.gd
@@ -1,5 +1,9 @@
 extends Control
 
+var ThemeR = load("res://assets/Styles/HealthBarRed.tres")
+var ThemeB = load("res://assets/Styles/HealthBarClassic.tres")
+var ThemeY = load("res://assets/Styles/HealthBarYellow.tres")
+
 func _ready() :
 	$hpBar.value = CustomSceneTree.getInstance()._playerManager.hp
 
@@ -10,7 +14,27 @@ func changeValue() :
 	if $hpBar.value >= 100 :
 		
 		$OverchargeBar.value = CustomSceneTree.getInstance()._playerManager.hp-100
+		
+		if $OverchargeBar.value >= 20 :
+			
+			
+			$OverchargeBar.set_theme(ThemeR)
+			$hpBar.set_theme(ThemeR)
+			
 	
 	else :
 		
 		$OverchargeBar.value = 0
+		$OverchargeBar.set_theme(ThemeY)	
+		
+		if($hpBar.value <= 50 and $hpBar.value > 20) :
+			
+			$hpBar.set_theme(ThemeY)
+			
+		elif ($hpBar.value <= 20) :
+			
+			$hpBar.set_theme(ThemeR)
+			
+		else :
+
+			$hpBar.set_theme(ThemeB)
