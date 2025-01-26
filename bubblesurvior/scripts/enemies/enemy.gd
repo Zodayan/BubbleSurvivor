3234c9c015086cdec0fd0530b4d87ba8f2ffd2a7
Dav314159 <davidjavet2@gmail.com>
2025-01-26 11:57:02 +0100

    bourssin pique pacontent

--- bubblesurvior/scripts/enemies/enemy.gd
+++ bubblesurvior/scripts/enemies/enemy.gd
@@ -80,6 +80,5 @@ func _on_body_entered(body: Node2D) -> void:
 		player_body = body
 
 func _on_body_exited(body: Node2D) -> void:
-	
 	if body.is_class("CharacterBody2D"):
 		is_collision_joueur = false
--- bubblesurvior/scripts/enemies/gros_michel.gd
+++ bubblesurvior/scripts/enemies/gros_michel.gd
@@ -1,9 +1,13 @@
 extends Enemy
 class_name GrosMichel
-
+@onready var animatedbody =$AnimatedSprite2D
 func _init() -> void:
 	super._init()
 	vitesse = 20
 	pv_max = 50
 	pv = pv_max
 	degats = 10
+	
+func _process(delta: float) -> void:
+	super._process(delta)
+	animatedbody.flip_v=false
